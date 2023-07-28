c48e7e83c54032083b8a020d0ad741cbda5ef547
snehalnikamfiserv <105724944+snehalnikamfiserv@users.noreply.github.com>
2023-07-28 11:21:54 +0530

    Create Program.cs
    added code in program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+public class Prgram
+{
+  int number1 = 10;
+  int number2 = 20;
+  int temp;
+
+  void swap()
+  {
+    temp = number1;
+    number1 = number2;
+    number2 = temp;
+
+    //Console.WriteLine("Swap numbers :" + )
+  }
+}
