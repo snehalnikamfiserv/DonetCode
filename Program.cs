85a3481587c7e3cbd303953b697912b9b60f15e0
Nikam, Snehal (Pune) <snehal.nikam@Fiserv.com>
2023-08-31 12:21:35 +0530

    added integer variable number3

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 using System;
 public class Prgram
 {
-  int number1 = 10;
-  int number2 = 20;
-  int temp;
+    int number1 = 10;
+    int number2 = 20;
+    int number3 = 30;
+    int temp;
 
-  void swap()
-  {
-    temp = number1;
-    number1 = number2;
-    number2 = temp;
+    void swap()
+    {
+        temp = number1;
+        number1 = number2;
+        number2 = temp;
 
-    //Console.WriteLine("Swap numbers :" + )
-  }
+        //Console.WriteLine("Swap numbers :" + )
+    }
 }
